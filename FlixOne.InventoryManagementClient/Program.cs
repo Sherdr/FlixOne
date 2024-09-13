3a19ceadd94127339bd82be502f10ae1a03fcf66
Sherdr <rainar_kuper@mail.ru>
2024-09-13 22:45:17 +0300

    Добавьте файлы проекта.

---
+++ FlixOne.InventoryManagementClient/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace FlixOne.InventoryManagementClient {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
