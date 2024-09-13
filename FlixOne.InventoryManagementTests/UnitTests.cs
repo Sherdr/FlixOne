1274a3fb17bffd0de9f76600bd79952f9ee8fc22
Sherdr <rainar_kuper@mail.ru>
2024-09-13 22:56:28 +0300

    Определение начальных модульных тестов.

---
+++ FlixOne.InventoryManagementTests/UnitTests.cs
@@ -0,0 +1,30 @@
+namespace FlixOne.InventoryManagementTests {
+
+    [TestClass]
+    public class UnitTests {
+        [TestMethod]
+        public void AddInventoryCommand_Successful() {
+            Assert.Inconclusive("AddInventoryCommand_Successful has not been implemented.");
+        }
+
+        [TestMethod]
+        public void GetInventoryCommand_Successful() {
+            Assert.Inconclusive("GetInventoryCommand_Successful has not been implemented.");
+        }
+
+        [TestMethod]
+        public void HelpCommand_Successful() {
+            Assert.Inconclusive("HelpCommand_Successful has not been implemented.");
+        }
+
+        [TestMethod]
+        public void QuitCommand_Successful() {
+            Assert.Inconclusive("QuitCommand_Successful has not been implemented.");
+        }
+
+        [TestMethod]
+        public void UpdateQuantityCommand_Successful() {
+            Assert.Inconclusive("UpdateQuantityCommand_Successful has not been implemented.");
+        }
+    }
+}
